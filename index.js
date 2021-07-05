bd6f4ec7ebf8b502c913f7aaf52acf86af1c6df7
Ivan Reed <ivanp.reed@gmail.com>
2021-07-05 18:09:33 -0500

    new homework holiday

---
+++ index.js
@@ -0,0 +1,12 @@
+let pizzaPlace = "Tonys"
+let numberOfToppings = 8
+
+console.log(pizzaPlace);
+console.log(numberOfToppings)
+
+console.log(`${pizzaPlace} gives numberOfToppings`)
+if (numberOfToppings < 10) {
+console.log("Quality, quantity")
+}
+if (numberOfToppings > 10) {
+console.log("A whole lof of pizza")}
